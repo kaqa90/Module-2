cd4a028f0d7411f91e5e591a31396c1ca91e272b
Khadija <khadija.k.qaraqe@gmail.com>
2016-11-02 08:57:23 +0200

    updates on Module 2

--- app.js
+++ app.js
@@ -5,7 +5,8 @@
          .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
          //To buy list
          	ToBuyController.$inject =['ShoppingListCheckOffService'];
-         	function ToBuyController (ShoppingListCheckOffService){
+         	function ToBuyController (ShoppingListCheckOffService)
+          {
          		var buy = this;
 
          		buy.items = ShoppingListCheckOffService.toBuyItems();
@@ -18,65 +19,67 @@
 
           //Already bought list
         	AlreadyBoughtController.$inject =['ShoppingListCheckOffService'];
-        	function AlreadyBoughtController (ShoppingListCheckOffService){
+        	function AlreadyBoughtController (ShoppingListCheckOffService)
+          {
         		var bought = this;
 
         		bought.items = ShoppingListCheckOffService.boughtItems();
         	}
           //Shopping list service
-        	function ShoppingListCheckOffService(){
+        	function ShoppingListCheckOffService()
+          {
         		var service = this;
 
-    var toBuyItems =
-    [
-    {name: "Cookies",
-     quantity: 10
-    },
-    {name: "Chocolate",
-     quantity: 3
-    },
-    {name: "Notebook",
-     quantity: 5
-    },
-    {name: "Pens",
-     quantity: 12
-    },
-    {name: "iPhones 7",
-     quantity: 4
-    },
-    {name: "MK bag",
-     quantity: 1
-    },
-    {name: "Laptop",
-     quantity: 4
-    },
-    {name: "Potato",
-     quantity: 8
-    },
-    {name: "Rings",
-     quantity: 2
-    },
-    {name: "Tissues",
-     quantity: 90
-    },
-    {name: "Glasses",
-     quantity: 17
-    }
-    ];
+            var toBuyItems =
+            [
+              {name: "Cookies",
+               quantity: 10
+              },
+              {name: "Chocolate",
+               quantity: 3
+              },
+              {name: "Notebook",
+               quantity: 5
+              },
+              {name: "Pens",
+               quantity: 12
+              },
+              {name: "iPhones 7",
+               quantity: 4
+              },
+              {name: "MK bag",
+               quantity: 1
+              },
+              {name: "Laptop",
+               quantity: 4
+              },
+              {name: "Potato",
+               quantity: 8
+              },
+              {name: "Rings",
+               quantity: 2
+              },
+              {name: "Tissues",
+               quantity: 90
+              },
+              {name: "Glasses",
+               quantity: 17
+              }
+            ];
 
-		var boughtItems = [];
+        		var boughtItems = [];
 
-  		service.bought = function(itemIndex) {
-  			boughtItems.push(toBuyItems[itemIndex]);
-  			toBuyItems.splice(itemIndex, 1);
-  		};
+          		service.bought = function(itemIndex) {
+          			boughtItems.push(toBuyItems[itemIndex]);
+          			toBuyItems.splice(itemIndex, 1);
+          		};
 
-  		service.boughtItems = function(){
-  			return boughtItems;
-  		};
+          		service.boughtItems = function(){
+          			return boughtItems;
+          		};
 
-  		service.buyItems = function(){
-  			return buyItems;
-  		};
-    }
+          		service.toBuyItems = function(){
+          			return toBuyItems;
+          		};
+          }
 })();
